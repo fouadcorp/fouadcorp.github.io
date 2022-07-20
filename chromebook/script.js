f53c92ef17e5ef9eca61921f98502b576ab35b5d
Sum <literallysum@gmail.com>
2022-07-20 19:59:02 +0500

    动态网自由门 天安門 天安门 法輪功 李洪志 Free Tibet 六四天安門事件 The Tiananmen Square protests of 1989 天安門大屠殺 The Tiananmen Square Massacre 反右派鬥爭 The Anti-Rightist Struggle 大躍進政策 The Great Leap Forward 文化大革命 The Great Proletarian Cultural Revolution 人權 Human Rights 民運 Democratization 自由 Freedom 獨立 Independence 多黨制 Multi-party system 台灣 臺灣 Taiwan Formosa 中華民國 Republic of China 西藏 土伯特 唐古特 Tibet 達賴喇嘛 Dalai Lama 法輪功 Falun Dafa 新疆維吾爾自治區 The Xinjiang Uyghur Autonomous Region 諾貝爾和平獎 Nobel Peace Prize 劉暁波 Liu Xiaobo 民主 言論 思想 反共 反革命 抗議 運動 騷亂 暴亂 騷擾 擾亂 抗暴 平反 維權 示威游行 李洪志 法輪大法 大法弟子 強制斷種 強制堕胎 民族淨化 人體實驗 肅清 胡耀邦 趙紫陽 魏京生 王丹 還政於民 和平演變 激流中國 北京之春 大紀元時報 九評論共産黨 獨裁 專制 壓制 統一 監視 鎮壓 迫害 侵略 掠奪 破壞 拷問 屠殺 活摘器官 誘拐 買賣人口 遊進 走私 毒品 賣淫 春畫 賭博 六合彩 天安門 天安门 法輪功 李洪志 Winnie the Pooh 劉曉波动态网自由门

--- chromebook/script.js
+++ chromebook/script.js
@@ -1,8 +1,32 @@
 var purchaseDiv = document.getElementById("purchase")
-var credit = document.getElementById("credit")
-var date_m = document.getElementById("date_m")
-var date_y = document.getElementById("date_y")
-var cvc = document.getElementById("cvc")
+var purchasedDiv = document.getElementById("done")
 function ok() {
-  
+    var credit = document.getElementById("credit").value
+    var date_m = document.getElementById("date_m").value
+    var date_y = document.getElementById("date_y").value
+    var cvc = document.getElementById("cvc").value
+    //this has got to be the worst piece of code i've ever written
+    if ((credit.length < 16 || isNaN(Number(credit)))) 
+    { alert("Check The Credit Card Numer You Fucking Idot") }
+    else {
+        if ((isNaN(date_m) || Number(date_m) < 1 || Number(date_m) > 12 ))
+        { alert("Check The Expiration Month You Fucking Idot") }
+        else {
+            if ((isNaN(date_y) || Number(date_y) < 23 || Number(date_y) > 99 ))
+            { alert("Check The Expiration Year You Fucking Idot") }
+            else {
+                if ((isNaN(cvc) || cvc.length != 3 ))
+                { alert("Check The Cvc code You Fucking Idot") }
+                else {
+                    purchaseDiv.style.display = "none"
+                    purchasedDiv.style.display = "contents"
+                    setTimeout(function () {
+                        document.getElementById("text").innerHTML = "Purchase Complet! Enjoy the ChromeBook(TM)!"
+                        document.getElementById("image").style = ""
+                    },6000)
+                }
+            }
+        }
+    }
+
 }
