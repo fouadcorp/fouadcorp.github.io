11b007edf46545c5fa8d6d16cfadd30caccde64f
notsu <literallysum@gmail.com>
2022-07-20 19:24:21 +0500

    Create script.js

---
+++ chromebook/script.js
@@ -0,0 +1,8 @@
+var purchaseDiv = document.getElementById("purchase")
+var credit = document.getElementById("credit")
+var date_m = document.getElementById("date_m")
+var date_y = document.getElementById("date_y")
+var cvc = document.getElementById("cvc")
+function ok() {
+  
+}
